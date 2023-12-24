29aab3a01729ad17839c6dff83109609ec1fc7fa
DAI ACTS <pnsharma681@gmail.com>
2023-12-24 15:51:03 +0530

    App Update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 
 app = Flask(__name__)
-UPLOAD_FOLDER = "Project/upload"
+UPLOAD_FOLDER = "upload"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
